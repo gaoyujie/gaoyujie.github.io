6d86f4607a947ebd1a039437a20aa83cf48f4c11
983809937 <983809937@qq.com>
2016-06-30 20:36:47 +0800

    all files upload

---
+++ mock/mock.js
@@ -0,0 +1,28 @@
+module.exports = {
+  rules: [
+    {
+      pattern: /\/api\/getLivelist.php\?rtype=origin$/,
+      respondwith: './livelist.json'
+    },
+    {
+      pattern: /\/api\/getLivelist.php\?rtype=more$/,
+      respondwith: './livelist-more.json'
+    },
+    {
+      pattern: /\/api\/getLivelist.php\?rtype=refresh$/,
+      respondwith: './livelist-refresh.json'
+    },
+    {
+      pattern: /\/api\/getLiveDetail.php\?id=1$/,
+      respondwith: './detail.json'
+    },
+    {
+      pattern: /\/api\/getLiveDetail.php\?id=2$/,
+      respondwith: './detail2.json'
+    },
+    {
+      pattern: /\/api\/getClass.php\?rtype=origin$/,
+      respondwith: './cate.json'
+    }
+  ]
+};
